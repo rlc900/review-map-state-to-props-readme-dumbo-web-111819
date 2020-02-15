35694a3df8768867672a1f444c572ff2515414bc
Rianna Cleary <rianna.cleary@gmail.com>
2020-02-15 16:16:27 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,12 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger
+  // because we are taking a part of the store's state and porting it to become props of the relevant component, we say that we are mapping it as props to the component, thus the name mapStateToProps.
+  // this return value is the value of props tat are added to the App component.
   return { items: state.items }
 }
 
+  // connect() function calls the mapStateToProps() function each time there is a change in state, and that mapStateToProps() receives state as its first argument.
+  // connect() takes whatever the return value is of the mapStateToProps() function and passes it to the component that is in those last set of parentheses (in this case, App).
 export default connect(mapStateToProps)(App);
